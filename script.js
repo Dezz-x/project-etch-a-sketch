e1eb0eaa52705a6d89fdb701be11ac3fb4fe4777
rdesmond21 <128184517+Dezz-x@users.noreply.github.com>
2024-06-10 20:59:13 -0400

    Container box and styling     - Added container to house the 16x16 boxes     - Added styling to background and h1 element

--- script.js
+++ script.js
@@ -1,1 +1,8 @@
+const container = document.querySelector(".container");
+
+const squareContainer = document.createElement("div");
+squareContainer.classList.add("square-container");
+
+container.appendChild(squareContainer);
+
 
