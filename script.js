bb488cbae67262a98e3eb873e335e5d3cd1ef540
rdesmond21 <128184517+Dezz-x@users.noreply.github.com>
2024-06-10 21:13:46 -0400

    Started creating boxes for grid

--- script.js
+++ script.js
@@ -1,8 +1,27 @@
 const container = document.querySelector(".container");
 
-const squareContainer = document.createElement("div");
-squareContainer.classList.add("square-container");
+const box1 = document.createElement("div");
+box1.classList.add("box");
 
-container.appendChild(squareContainer);
+container.appendChild(box1);
+
+const box2 = document.createElement("div");
+box2.classList.add("box");
+
+container.appendChild(box2);
+
+const box3 = document.createElement("div");
+box3.classList.add("box");
+
+container.appendChild(box3);
+
+const box4 = document.createElement("div");
+box4.classList.add("box");
+
+container.appendChild(box4);
+
+
+
+
 
 
