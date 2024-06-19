466c3ca1d47cfb5bfab985a57efcc1079ba9447b
rdesmond21 <128184517+Dezz-x@users.noreply.github.com>
2024-06-18 21:49:40 -0400

    Creating a grid with flexbox     - Still figuring out how to create a grid using flexbox     - Plenty of refactoring to go

--- script.js
+++ script.js
@@ -1,19 +1,33 @@
 const container = document.querySelector(".container");
 
-function createRow(){
+/*function createRow(){
     for(let i = 0; i < 16; i++){
-        const square = document.createElement("div");
-        square.classList.add("square");
+        for(let j = 0; j < 16; j++){
+            const square = document.createElement("div");
+            square.classList.add("square");
     
-        container.appendChild(square);
+            container.appendChild(square);
+        };
     };
 };
+createRow();*/
+
+function createRow(){
+    for(let i = 0; i < 16; i++){
+        const rows = document.createElement("div");
+        rows.classList.add("rows");
+
+        container.appendChild(rows);
+    };
+};
+createRow();
 
 function createColumn(){
-    for(let i = 0; i < 16; i++){
-        createRow();
+    for(let j = 0; j < 16; j++){
+        const columns = document.createElement("div");
+        columns.classList.add("columns")
+
+        container.appendChild(columns);
     };
 };
 createColumn();
-
-
